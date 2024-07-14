64b8c31def06d00fb0e24f73d252c0054b1e3e84
Simon Larsson <simon.larsson@nordlo.com>
2024-07-14 19:58:13 +0200

    Validation of authentication

--- custom_components/greenely/config_flow.py
+++ custom_components/greenely/config_flow.py
@@ -17,6 +17,8 @@ from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.exceptions import HomeAssistantError
 
+from .api import GreenelyApi
+
 from .const import (
     DOMAIN,
     GREENELY_DAILY_PRODUCED_ELECTRICITY,
@@ -54,7 +56,8 @@ class PlaceholderHub:
 
     async def authenticate(self, email: str, password: str) -> bool:
         """Test if we can authenticate with the host."""
-        return True
+        api = GreenelyApi(email, password)
+        return await api.check_auth()
 
 
 async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
@@ -62,23 +65,11 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data[CONF_USERNAME], data[CONF_PASSWORD]
-    # )
 
     hub = PlaceholderHub(data[CONF_EMAIL])
 
     if not await hub.authenticate(data[CONF_EMAIL], data[CONF_PASSWORD]):
         raise InvalidAuth
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
 
     # Return info that you want to store in the config entry.
     return {"title": "Greenely Config"}
@@ -105,8 +96,6 @@ class ConfigFlow(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             try:
                 info = await validate_input(self.hass, user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
             except InvalidAuth:
                 errors["base"] = "invalid_auth"
             except Exception:
@@ -201,11 +190,6 @@ class GreenelyOptionsFlow(OptionsFlow):
                 ): bool,
             }
         )
-
-
-class CannotConnect(HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
